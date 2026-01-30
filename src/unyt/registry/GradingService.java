c0396024b41ee5bf35beb4aec7cc27f4152d1e24
elioraaa <elioraqendro2021@gmail.com>
2026-01-30 15:38:46 +0100

    added gradingservice.java

---
+++ src/unyt/registry/GradingService.java
@@ -0,0 +1,32 @@
+package unyt.registry;
+
+public class GradingService {
+    public LetterGrade letterFor(Level level, int numeric) {
+        if (numeric <0 || numeric 100) throw new IllegalArgumentExeption("invalid grade");
+
+        if (level == Level.UG) {
+            if (numeric >= 95) return LetterGrade.A;
+            if (numeric >= 90) return LetterGrade.A_MINUS;
+            if (numeric >= 87) return LetterGrade.B_PLUS;
+            if (numeric >= 84) return LetterGrade.B;
+            if (numeric >= 80) return LetterGrade.B_MINUS;
+            if (numeric >= 77) return LetterGrade.C_PLUS;
+            if (numeric >= 74) return LetterGrade.C;
+            if (numeric >= 70) return LetterGrade.C_MINUS;
+            if (numeric >= 67) return LetterGrade.D_PLUS;
+            if (numeric >= 64) return LetterGrade.D;
+            if (numeric >= 60) return LetterGrade.D_MINUS;
+            return LetterGrade.F;
+        } else {
+            if (numeric >= 90) return LetterGrade.A;
+            if (numeric >= 85) return LetterGrade.B_PLUS;
+            if (numeric >= 80) return LetterGrade.B;
+            if (numeric >= 75) return LetterGrade.C;
+            return LetterGrade.F;
+        }
+    }
+
+    public static double round2(double x){
+        return Math.round(x* 100.0) / 100.0;
+    }
+}
